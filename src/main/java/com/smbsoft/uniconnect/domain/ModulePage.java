11024b5ba00771fd40d6fcca28eea69c1494438f
smb564 <supun.14@cse.mrt.ac.lk>
2017-03-26 23:28:23 +0530

    Added module page, and completed admin functions

---
+++ src/main/java/com/smbsoft/uniconnect/domain/ModulePage.java
@@ -0,0 +1,133 @@
+package com.smbsoft.uniconnect.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A ModulePage.
+ */
+
+@Document(collection = "module_page")
+public class ModulePage implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    @Size(min = 5, max = 40)
+    @Field("title")
+    private String title;
+
+    @Size(min = 5, max = 150)
+    @Field("description")
+    private String description;
+
+    @NotNull
+    @Field("module_code")
+    private String moduleCode;
+
+    @Field("targets")
+    private String[][] targets;
+
+    @Field("posts")
+    private String[] posts;
+
+    public String[][] getTargets() {
+        return targets;
+    }
+
+    public void setTargets(String[][] targets) {
+        this.targets = targets;
+    }
+
+    public String[] getPosts() {
+        return posts;
+    }
+
+    public void setPosts(String[] posts) {
+        this.posts = posts;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public ModulePage title(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ModulePage description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getModuleCode() {
+        return moduleCode;
+    }
+
+    public ModulePage moduleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+        return this;
+    }
+
+    public void setModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ModulePage modulePage = (ModulePage) o;
+        if (modulePage.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, modulePage.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ModulePage{" +
+            "id=" + id +
+            ", title='" + title + "'" +
+            ", description='" + description + "'" +
+            ", moduleCode='" + moduleCode + "'" +
+            '}';
+    }
+}
